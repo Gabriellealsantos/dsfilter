a93dc65cd98dcddaffce1b53c7b440e88a5aabdd
Gabriel <gabriel.lealsantos@hotmail.com>
2024-06-05 10:41:05 -0300

    Onfilter Event

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,28 +1,63 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import { useState } from 'react';
 import './styles.css';
 
-export default function Filter() {
+type Props = {
+    onFilter: Function
+}
+
+type FormData = {
+    min?: number,
+    max?: number
+}
+
+export default function Filter({ onFilter }: Props) {
+
+
+    const [formData, setFormData] = useState<FormData>({});
+
+    function handleInputChange(event : React.ChangeEvent<HTMLInputElement>) {
+        const value = event.target.value;
+        const name = event.target.name;
+        setFormData({...formData, [name] : value})
+
+    }
+
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const min = formData.min || 0;
+        const max = formData.max || Number.MAX_VALUE;
+        onFilter(min, max);
+    }
+
     return (
         <div className="card">
             <div className="container-card">
 
-                <form>
+                <form onSubmit={handleSubmit}>
 
                     <div className="ds-search-bar ds-mb20">
                         <input
+                            name='min'
+                            value={formData.min || ""}
                             type="text"
                             placeholder="Preço mínimo"
+                            onChange={handleInputChange}
                         />
                     </div>
 
                     <div className="ds-search-bar ds-mb20">
                         <input
+                            name='max'
+                            value={formData.max || ""}
                             type="text"
                             placeholder="Preço máximo"
+                            onChange={handleInputChange}
                         />
                     </div>
 
                     <div>
-                        <button className='ds-btn ds-search-btn'>Filtrar</button>
+                        <button type='submit' className='ds-btn ds-search-btn'>Filtrar</button>
                     </div>
 
 
--- src/components/ListingBody/index.tsx
+++ src/components/ListingBody/index.tsx
@@ -4,24 +4,38 @@ import Filter from "../Filter";
 import Listing from "../Listing";
 import { findByPrice } from "../../services/product-service";
 
+type QuaryParams = {
+    min: number;
+    max: number;
+}
 
 export default function ListingBody() {
 
     const [products, setProducts] = useState<ProductDTO[]>([]);
 
+    const [quaryParams, setQuaryParams] = useState<QuaryParams>({
+        min: 0,
+        max: Number.MAX_VALUE
+    });
+
     useEffect(() => {
         const fetchProducts = async () => {
-            const productsData = await findByPrice(0, 5000);
+            const productsData = await findByPrice(quaryParams.min, quaryParams.max);
             setProducts(productsData);
         };
         fetchProducts();
-    }, []);
+    }, [quaryParams]);
+
+    function handleFilter(min: number, max: number) {
+        setProducts([]);
+        setQuaryParams({ ...quaryParams, min: min, max: max })
+    }
 
 
     return (
-        
+
         <div className="ds-container">
-            <Filter />
+            <Filter onFilter={handleFilter} />
             <Listing products={products} />
         </div>
 
